142edc6a6537c8b805f820c369dedf655a2bee4d
qsx <qsx>
2023-08-29 17:16:27 -0400

    passporting added

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = (0, express_1.default)();
 const port = 5000;
-app.get('/', (req, res) => {
-    res.send({ hi: 'theres' });
-});
+const keys = require('../keys');
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+    console.log(accessToken);
+    // You can also use other parameters like refreshToken, profile, etc.
+}));
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
+app.get('/auth/google/callback', passport.authenticate('google'));
 app.listen(port, () => {
     console.log(`Running on ${port}`);
 });
