c7fd8aaeabdd4349a51f1af39399a3d6bb1f225c
qsx <qsx>
2023-09-01 10:51:42 -0400

    add loout flow

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const passport_1 = __importDefault(require("passport"));
 dotenv_1.default.config();
 const cookie_session_1 = __importDefault(require("cookie-session"));
 const mongoose = require('mongoose');
@@ -16,6 +17,8 @@ if (process.env.cookieKey == null) {
     throw new Error('cookieKey environment variable is not set');
 }
 app.use((0, cookie_session_1.default)({ maxAge: 30 * 24 * 60 * 60 * 1000, keys: [process.env.cookieKey] }));
+app.use(passport_1.default.initialize());
+app.use(passport_1.default.session());
 require('./routes/authRoutes')(app);
 const port = 5000;
 app.listen(port, () => {
